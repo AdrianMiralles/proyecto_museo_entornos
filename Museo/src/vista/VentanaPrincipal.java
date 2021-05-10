f5b1f3d72a93f6b200feba16a1af230ac53d67b9
victor <victormanuel.caballero@juanxxiii.net>
2021-05-10 18:16:40 +0200

    Desarrollo, aún por terminar, de los paneles de añadir y consultar

--- Museo/src/vista/VentanaPrincipal.java
+++ Museo/src/vista/VentanaPrincipal.java
@@ -10,6 +10,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import controlador.BaseDeDatos;
+import modelo.Autor;
 import modelo.Obra;
 
 import javax.swing.JMenuBar;
@@ -28,6 +29,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JComboBox;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -39,6 +41,15 @@ public class VentanaPrincipal extends JFrame {
 	private JTable tablaConsultas;
 	DefaultTableModel modeloTabla = new DefaultTableModel();
 	private JButton btnTitulo;
+	private JButton btnTipo;
+	private JButton btnFecha;
+	private JButton btnLugar;
+	private JTextField txtTituloObra;
+	private JTextField txtTipoObra;
+	private JTextField txtLugarObra;
+	private JTextField txtFechaObra;
+	private JComboBox cmbAutores;
+	private JTextField txtAutoresObra;
 
 	/**
 	 * Launch the application.
@@ -62,35 +73,35 @@ public class VentanaPrincipal extends JFrame {
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 802, 453);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnPrincipal = new JMenu("Menu");
 		menuBar.add(mnPrincipal);
-		
+
 		JButton btnConsultar = new JButton("Consultar");
 		btnConsultar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				CardLayout c=(CardLayout)(contentPane.getLayout());
+				CardLayout c = (CardLayout) (contentPane.getLayout());
 				c.show(contentPane, "t2");
 			}
 		});
-		
+
 		JMenu mnAnanirVar = new JMenu("A\u00F1adir");
 		mnPrincipal.add(mnAnanirVar);
-		
+
 		JButton btnAnadirAutor = new JButton("A\u00F1adir Autor");
 		mnAnanirVar.add(btnAnadirAutor);
-		
+
 		JButton btnNewButton_1 = new JButton("A\u00F1adir Obra");
 		mnAnanirVar.add(btnNewButton_1);
 		mnPrincipal.add(btnConsultar);
-		
+
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				CardLayout c=(CardLayout)(contentPane.getLayout());
+				CardLayout c = (CardLayout) (contentPane.getLayout());
 				c.show(contentPane, "t3");
 			}
 		});
@@ -99,52 +110,149 @@ public class VentanaPrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new CardLayout(0, 0));
-		
+
 		JPanel pAnadir = new JPanel();
 		contentPane.add(pAnadir, "t1");
 		pAnadir.setLayout(null);
-		
+
 		JButton btnLimpiar = new JButton("A\u00F1adir");
 		btnLimpiar.setBounds(687, 358, 89, 23);
 		pAnadir.add(btnLimpiar);
-		
+
 		JButton btnAnadir = new JButton("Limpiar ");
 		btnAnadir.setBounds(0, 358, 89, 23);
 		pAnadir.add(btnAnadir);
-		
+
+		JLabel lblTituloVentanaAniadir = new JLabel(
+				"Si desea a\u00F1adir una nueva obra rellene todos los campos correctamente.");
+		lblTituloVentanaAniadir.setBounds(10, 11, 376, 14);
+		pAnadir.add(lblTituloVentanaAniadir);
+
+		JLabel lblTituloObra = new JLabel("T\u00EDtulo de la obra");
+		lblTituloObra.setBounds(20, 36, 150, 14);
+		pAnadir.add(lblTituloObra);
+
+		txtTituloObra = new JTextField();
+		txtTituloObra.setBounds(20, 61, 150, 20);
+		pAnadir.add(txtTituloObra);
+		txtTituloObra.setColumns(10);
+
+		JLabel lblTipoObra = new JLabel("Tipo de obra");
+		lblTipoObra.setBounds(20, 92, 150, 14);
+		pAnadir.add(lblTipoObra);
+
+		txtTipoObra = new JTextField();
+		txtTipoObra.setBounds(20, 117, 150, 20);
+		pAnadir.add(txtTipoObra);
+		txtTipoObra.setColumns(10);
+
+		JLabel lblFechaObra = new JLabel("Fecha de creaci\u00F3n");
+		lblFechaObra.setBounds(220, 36, 150, 14);
+		pAnadir.add(lblFechaObra);
+
+		JLabel lblLugarObra = new JLabel("Ubicaci\u00F3n de la obra");
+		lblLugarObra.setBounds(220, 92, 150, 14);
+		pAnadir.add(lblLugarObra);
+
+		txtLugarObra = new JTextField();
+		txtLugarObra.setColumns(10);
+		txtLugarObra.setBounds(220, 117, 150, 20);
+		pAnadir.add(txtLugarObra);
+
+		txtFechaObra = new JTextField();
+		txtFechaObra.setColumns(10);
+		txtFechaObra.setBounds(220, 61, 150, 20);
+		pAnadir.add(txtFechaObra);
+
+		cmbAutores = new JComboBox();
+		cmbAutores.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+			}
+		});
+		cmbAutores.setBounds(20, 173, 150, 20);
+		pAnadir.add(cmbAutores);
+
+		JLabel lblAutorObra = new JLabel("Autor de la obra");
+		lblAutorObra.setBounds(20, 148, 150, 14);
+		pAnadir.add(lblAutorObra);
+
+		txtAutoresObra = new JTextField();
+		txtAutoresObra.setEnabled(false);
+		txtAutoresObra.setBounds(20, 204, 150, 20);
+		pAnadir.add(txtAutoresObra);
+		txtAutoresObra.setColumns(10);
+
 		JPanel pConsultar = new JPanel();
 		contentPane.add(pConsultar, "t2");
 		pConsultar.setLayout(null);
-		
-		JLabel lblTituloConsultar = new JLabel("Seleccione un par\u00E1metro de b\u00FAsqueda para la consulta");
-		lblTituloConsultar.setBounds(10, 11, 290, 14);
+
+		JLabel lblTituloConsultar = new JLabel(
+				"Seleccione un par\u00E1metro de b\u00FAsqueda para la consulta y escriba su consulta");
+		lblTituloConsultar.setBounds(10, 11, 415, 14);
 		pConsultar.add(lblTituloConsultar);
-		
+
 		JRadioButton rdbtnTituloObra = new JRadioButton("T\u00EDtulo de la obra");
+		rdbtnTituloObra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnTitulo.setVisible(true);
+				btnTipo.setVisible(false);
+				btnFecha.setVisible(false);
+				btnLugar.setVisible(false);
+				txtBuscar.setVisible(true);
+			}
+		});
 		buttonGroup.add(rdbtnTituloObra);
-		rdbtnTituloObra.setBounds(10, 32, 109, 23);
+		rdbtnTituloObra.setBounds(10, 32, 120, 23);
 		pConsultar.add(rdbtnTituloObra);
-		
+
 		JRadioButton rdbtnTipoObra = new JRadioButton("Tipo de obra");
+		rdbtnTipoObra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnTitulo.setVisible(false);
+				btnTipo.setVisible(true);
+				btnFecha.setVisible(false);
+				btnLugar.setVisible(false);
+				txtBuscar.setVisible(true);
+			}
+		});
 		buttonGroup.add(rdbtnTipoObra);
-		rdbtnTipoObra.setBounds(121, 32, 109, 23);
+		rdbtnTipoObra.setBounds(132, 32, 109, 23);
 		pConsultar.add(rdbtnTipoObra);
-		
+
 		JRadioButton rdbtnFecha = new JRadioButton("Fecha");
+		rdbtnFecha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnTitulo.setVisible(false);
+				btnTipo.setVisible(false);
+				btnFecha.setVisible(true);
+				btnLugar.setVisible(false);
+				txtBuscar.setVisible(true);
+			}
+		});
 		buttonGroup.add(rdbtnFecha);
-		rdbtnFecha.setBounds(232, 32, 109, 23);
+		rdbtnFecha.setBounds(243, 32, 80, 23);
 		pConsultar.add(rdbtnFecha);
-		
+
 		JRadioButton rdbtnUbicacion = new JRadioButton("Ubicaci\u00F3n de la obra");
+		rdbtnUbicacion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnTitulo.setVisible(false);
+				btnTipo.setVisible(false);
+				btnFecha.setVisible(false);
+				btnLugar.setVisible(true);
+				txtBuscar.setVisible(true);
+			}
+		});
 		buttonGroup.add(rdbtnUbicacion);
-		rdbtnUbicacion.setBounds(343, 32, 135, 23);
+		rdbtnUbicacion.setBounds(325, 32, 150, 23);
 		pConsultar.add(rdbtnUbicacion);
-		
+
 		txtBuscar = new JTextField();
 		txtBuscar.setBounds(10, 62, 150, 20);
 		pConsultar.add(txtBuscar);
 		txtBuscar.setColumns(10);
-		
+
 		btnTitulo = new JButton("Consultar");
 		btnTitulo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -152,16 +260,15 @@ public class VentanaPrincipal extends JFrame {
 					JOptionPane.showMessageDialog(null, "Compruebe que la caja de texto no esté vacía.", "Alerta",
 							JOptionPane.WARNING_MESSAGE);
 				} else {
-					
+
 					listadoObras = bbdd.consulta("titulo", txtBuscar.getText());// llamo al método
-																								// pasándole un
-																								// parámetro con el
-																								// texto
+																				// pasándole un
+																				// parámetro con el
+																				// texto
 					// específico y guardo el arraylist que devuelve para
 					// despues volcarlo en un JTable
 
-					modeloTabla.setColumnIdentifiers(
-							new Object[] { "Título", "Tipo", "Fecha", "Ubicación" });
+					modeloTabla.setColumnIdentifiers(new Object[] { "Título", "Tipo", "Fecha", "Ubicación" });
 					modeloTabla.setRowCount(0);
 					tablaConsultas.setModel(modeloTabla);
 
@@ -169,25 +276,137 @@ public class VentanaPrincipal extends JFrame {
 					while (iterador.hasNext()) {
 						Obra elemento = iterador.next();
 						modeloTabla.addRow(new Object[] { // carga la tabla con los elementos indicados
-								elemento.getTitulo(), elemento.getTipo(), elemento.getFecha(),
-								elemento.getLugar() });
+								elemento.getTitulo(), elemento.getTipo(), elemento.getFecha(), elemento.getLugar() });
 					}
 				}
 			}
 		});
 		btnTitulo.setBounds(170, 61, 89, 23);
 		pConsultar.add(btnTitulo);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 93, 756, 278);
 		pConsultar.add(scrollPane);
-		
+
 		tablaConsultas = new JTable();
 		scrollPane.setColumnHeaderView(tablaConsultas);
 		scrollPane.setViewportView(tablaConsultas);
-		
+
+		btnTipo = new JButton("Consultar");
+		btnTipo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (txtBuscar.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Compruebe que la caja de texto no esté vacía.", "Alerta",
+							JOptionPane.WARNING_MESSAGE);
+				} else {
+
+					listadoObras = bbdd.consulta("tipo", txtBuscar.getText());// llamo al método
+																				// pasándole un
+																				// parámetro con el
+																				// texto
+					// específico y guardo el arraylist que devuelve para
+					// despues volcarlo en un JTable
+
+					modeloTabla.setColumnIdentifiers(new Object[] { "Título", "Tipo", "Fecha", "Ubicación" });
+					modeloTabla.setRowCount(0);
+					tablaConsultas.setModel(modeloTabla);
+
+					Iterator<Obra> iterador = listadoObras.iterator();
+					while (iterador.hasNext()) {
+						Obra elemento = iterador.next();
+						modeloTabla.addRow(new Object[] { // carga la tabla con los elementos indicados
+								elemento.getTitulo(), elemento.getTipo(), elemento.getFecha(), elemento.getLugar() });
+					}
+				}
+			}
+		});
+		btnTipo.setBounds(170, 61, 89, 23);
+		pConsultar.add(btnTipo);
+
+		btnFecha = new JButton("Consultar");
+		btnFecha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (txtBuscar.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Compruebe que la caja de texto no esté vacía.", "Alerta",
+							JOptionPane.WARNING_MESSAGE);
+				} else {
+
+					listadoObras = bbdd.consulta("fecha", txtBuscar.getText());// llamo al método
+																				// pasándole un
+																				// parámetro con el
+																				// texto
+					// específico y guardo el arraylist que devuelve para
+					// despues volcarlo en un JTable
+
+					modeloTabla.setColumnIdentifiers(new Object[] { "Título", "Tipo", "Fecha", "Ubicación" });
+					modeloTabla.setRowCount(0);
+					tablaConsultas.setModel(modeloTabla);
+
+					Iterator<Obra> iterador = listadoObras.iterator();
+					while (iterador.hasNext()) {
+						Obra elemento = iterador.next();
+						modeloTabla.addRow(new Object[] { // carga la tabla con los elementos indicados
+								elemento.getTitulo(), elemento.getTipo(), elemento.getFecha(), elemento.getLugar() });
+					}
+				}
+			}
+		});
+		btnFecha.setBounds(170, 62, 89, 23);
+		pConsultar.add(btnFecha);
+
+		btnLugar = new JButton("Consultar");
+		btnLugar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (txtBuscar.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Compruebe que la caja de texto no esté vacía.", "Alerta",
+							JOptionPane.WARNING_MESSAGE);
+				} else {
+
+					listadoObras = bbdd.consulta("lugar", txtBuscar.getText());// llamo al método
+																				// pasándole un
+																				// parámetro con el
+																				// texto
+					// específico y guardo el arraylist que devuelve para
+					// despues volcarlo en un JTable
+
+					modeloTabla.setColumnIdentifiers(new Object[] { "Título", "Tipo", "Fecha", "Ubicación" });
+					modeloTabla.setRowCount(0);
+					tablaConsultas.setModel(modeloTabla);
+
+					Iterator<Obra> iterador = listadoObras.iterator();
+					while (iterador.hasNext()) {
+						Obra elemento = iterador.next();
+						modeloTabla.addRow(new Object[] { // carga la tabla con los elementos indicados
+								elemento.getTitulo(), elemento.getTipo(), elemento.getFecha(), elemento.getLugar() });
+					}
+				}
+			}
+		});
+		btnLugar.setBounds(170, 62, 89, 23);
+		pConsultar.add(btnLugar);
+
 		JPanel pSalir = new JPanel();
 		contentPane.add(pSalir, "t3");
 		pSalir.setLayout(null);
+
+		// Oculto botones por las opciones de los radiobutton
+		btnTitulo.setVisible(false);
+		btnTipo.setVisible(false);
+		btnFecha.setVisible(false);
+		btnLugar.setVisible(false);
+		txtBuscar.setVisible(false);
+		cargandoComboPlatforms();
+	}
+
+	public void cargandoComboPlatforms() {
+
+		ArrayList<Autor> arrRegistro;
+		arrRegistro = bbdd.cargandoComboAutores();
+
+		Iterator<Autor> iterador = arrRegistro.iterator();
+		while (iterador.hasNext()) {
+			Autor elemento = iterador.next();
+			cmbAutores.addItem(elemento.getIdAutor() + ", " + elemento.getNombreAutor());
+		}
 	}
 }
