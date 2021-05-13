bfa37bde40a6612613c6e54af51bd3bc99aec3f8
victor.caballero <victor.caballero@AULA1-16.JUANXXIII.net>
2021-05-13 19:18:34 +0200

    Clase fichero completa

--- Museo/src/controlador/Fichero.java
+++ Museo/src/controlador/Fichero.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.swing.JOptionPane;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -13,18 +15,16 @@ import org.apache.poi.ss.usermodel.Workbook;
 
 import modelo.Obra;
 
-
-
-public class Fichero {
+public class Fichero  {
 	public void generarXLS(ArrayList<Obra> consulta, String nombre) {
-		//Crea un libro
+		// Crea un libro
 		Workbook workbook = new HSSFWorkbook();
 		// Crea hoja nueva
 		Sheet sheet = workbook.createSheet("Hoja de datos");
 
 		Iterator<Obra> iterador = consulta.iterator();
-		int numeroRenglon = 0;//inicio de filas
-	
+		int numeroRenglon = 0;// inicio de filas
+
 		Row row = sheet.createRow(numeroRenglon++);
 		Cell cell = row.createCell(0);
 		cell.setCellValue("ID-Obra");
@@ -36,16 +36,17 @@ public class Fichero {
 		cell.setCellValue("Fecha de creación");
 		cell = row.createCell(3);
 		cell.setCellValue("Ubicación actual");
+
 		while (iterador.hasNext()) {
-			int numeroCelda = 0;//Número de inicio de celdas
+			int numeroCelda = 0;// Número de inicio de celdas
 			Obra elemento = iterador.next();
-			row = sheet.createRow(numeroRenglon++);//cada vez que entra al while avanzo una fila
-			
+			row = sheet.createRow(numeroRenglon++);// cada vez que entra al while avanzo una fila
+
 			cell = row.createCell(numeroCelda++);
 			cell.setCellValue(elemento.getIdObra());
 			cell = row.createCell(numeroCelda++);
 			cell.setCellValue(elemento.getTitulo());
-			cell = row.createCell(numeroCelda++);//cada vez que escribe una celda avanzo una celda
+			cell = row.createCell(numeroCelda++);// cada vez que escribe una celda avanzo una celda
 			cell.setCellValue(elemento.getTipo());
 			cell = row.createCell(numeroCelda++);
 			cell.setCellValue(elemento.getFecha());
@@ -59,8 +60,10 @@ public class Fichero {
 			FileOutputStream fichero = new FileOutputStream(new File(nombre + "Consulta.xls"));
 			workbook.write(fichero);
 			fichero.close();
+			JOptionPane.showMessageDialog(null, "Archivo creado con éxito", "Aviso", JOptionPane.PLAIN_MESSAGE);
 		} catch (Exception e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "El archivo xls no ha podido ser creado por un error", "Error",
+					JOptionPane.ERROR_MESSAGE);
 		}
 
 	}
